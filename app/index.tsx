6efc2aafbca73eeb7a62ea3d2fb8fc459e4e1399
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2026-02-18 15:38:38 +0100

    feat: Add an animated splash screen and update the backend URL in the environment configuration.

--- app/index.tsx
+++ app/index.tsx
@@ -51,7 +51,7 @@ export default function Index() {
         ]),
         Animated.delay(1500),
       ]).start(() => {
-        router.replace('/screens/complete-profile');
+        router.replace('/screens/auth/login');
       });
     }
   }, [fontsLoaded]);
